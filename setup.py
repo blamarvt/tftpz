93852a1c74c0858ffb39bb1a30c47f5e7a5361f5
vonhollen <nicholas.vonhollen@rackspace.com>
2010-09-22 16:25:51 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+		name="tftpz",
+		version="0.1",
+		author="Brian Lamar",
+		author_email="brian.lamar@rackspace.com",
+		maintainer="Nicholas VonHollen",
+		maintainer_email="nicholas.vonhollen@rackspace.com",
+		license="Apache License 2.0",
+		packages=['tftpz'],
+		package_dir={"":"src/py"},
+		data_files=[('/etc/init.d', ['src/init.d/tftpz'])],
+		entry_points="""
+		[console_scripts]
+                tftpz=tftpz:main
+		"""
+		)
